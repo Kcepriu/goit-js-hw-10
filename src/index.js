6dfdb73b04572584abd4717b8aa381c43591918f
Kcepriu <kcepriu@gmail.com>
2022-12-17 20:10:32 +0200

    ended task

---
+++ src/index.js
@@ -0,0 +1,100 @@
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import FetchCountries from './js/fetchCountries';
+import TemplateHTML from './js/templateHTML';
+_ = require('lodash');
+
+const DEBOUNCE_DELAY = 300;
+const TEXTS = {
+  text_failure: 'Oops, there is no country with that name',
+  text_error_connect: 'Error connect to server',
+  text_info: 'Too many matches found. Please enter a more specific name.',
+};
+
+const fetchCountries = new FetchCountries();
+const templateHTML = new TemplateHTML();
+
+const refs = {
+  inputCountry: document.querySelector('#search-box'),
+  listCountries: document.querySelector('.country-list'),
+  infoCoutry: document.querySelector('.country-info'),
+};
+
+addEvetns();
+
+// * EVENTS
+function addEvetns() {
+  refs.inputCountry.addEventListener(
+    'keydown',
+    _.debounce(finesetWriteText, DEBOUNCE_DELAY)
+  );
+}
+
+function finesetWriteText() {
+  const nameCountry = getTextInputCountry();
+
+  if (nameCountry === '') {
+    //Якщо пусте поле, то очистити Вивід країн
+    resetFieldsInfoCountry();
+    //Забудемо останній пошук
+    fetchCountries.nameLastFindCountry = '';
+    return;
+  } else if (nameCountry === fetchCountries.nameLastFindCountry) {
+    //Якщо поле не змінилося (додали пробіл чи тикнули якусь службову кнопку) то нічого не рорбити
+    return;
+  }
+  //Інакше запустити пошук країни
+  resetFieldsInfoCountry();
+  startFindCountry(nameCountry);
+}
+
+// *  Field this window
+function getTextInputCountry() {
+  return refs.inputCountry.value.trim();
+}
+
+function resetFieldsInfoCountry() {
+  //очистити Вивід країн
+  refs.listCountries.innerHTML = '';
+  refs.infoCoutry.innerHTML = '';
+}
+
+function showInformationOneCountry(data) {
+  refs.infoCoutry.innerHTML = templateHTML.getHtmlTextCountry(data);
+}
+
+function showListCountry(arrayCountries) {
+  refs.listCountries.innerHTML =
+    templateHTML.getHtmlTextItemsListCountry(arrayCountries);
+}
+
+// * NOTIFICATION
+function showFailure(message) {
+  Notify.failure(message);
+}
+
+function showInfo(message) {
+  Notify.info(message);
+}
+
+// * FIND COUNTRY
+function startFindCountry(nameCountry) {
+  fetchCountries
+    .findCountry(nameCountry)
+    .then(data => {
+      fetchCountries.nameLastFindCountry = nameCountry;
+      showInformation(data);
+    })
+    .catch(showError);
+}
+
+function showInformation(data) {
+  if (data.length > 10) showInfo(TEXTS.text_info);
+  else if (data.length === 1) showInformationOneCountry(data[0]);
+  else showListCountry(data);
+}
+
+function showError(error) {
+  if (error === fetchCountries.ERROR_NOT_FOUND) showFailure(TEXTS.text_failure);
+  else showFailure(TEXTS.text_error_connect);
+}
---
+++ src/js/templateHTML.js
@@ -0,0 +1,66 @@
+import { isArray } from 'lodash';
+
+export default class TemplateHTML {
+  // * List countries
+  getHtmlTextItemsListCountry(arrayCountries) {
+    const strHTML = arrayCountries.reduce((strHTML, value) => {
+      strHTML += this.getHtmlTextItemListCountry(value);
+      return strHTML;
+    }, '');
+
+    return strHTML;
+  }
+
+  getHtmlTextItemListCountry(data) {
+    const {
+      name: { official: nameCountry },
+      flags: { svg: srcFlag },
+    } = data;
+
+    return `
+  <li class="item_list_country">
+    <img src="${srcFlag}" alt="Flag ${nameCountry}" width="30" />
+    <span>${nameCountry}</span>
+  </li>
+  `;
+  }
+  // * One country
+  getHtmlTextCountry(data) {
+    const {
+      name: { official: nameCountry },
+      flags: { svg: srcFlag },
+      capital: [nameCapital],
+      population,
+      languages,
+    } = data;
+
+    let strHTML = this.getTemplateNameCOuntry(nameCountry, srcFlag);
+
+    strHTML += this.getTemplateInformationCoutry('Capital', nameCapital);
+    strHTML += this.getTemplateInformationCoutry('Population', population);
+    strHTML += this.getTemplateInformationCoutry(
+      'Languages',
+      this.langToStr(languages)
+    );
+
+    return strHTML;
+  }
+
+  getTemplateNameCOuntry(nameCountry, srcFlag) {
+    return `<div class="title_country">
+           <img src="${srcFlag}" alt="Flag ${nameCountry}" width="30" />
+           <span class="name__country">${nameCountry}</span>
+       </div>`;
+  }
+  getTemplateInformationCoutry(nameInformation, valueInformation) {
+    return `<p class="info__city">
+          <span class="info__city--parametr">${nameInformation}:</span>${valueInformation}
+        </p>`;
+  }
+  langToStr(languages) {
+    return Object.values(languages).reduce(
+      (str, value) => (str += (str ? ', ' : '') + value),
+      ''
+    );
+  }
+}
